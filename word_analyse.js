7d4630b48659e39a568dc3838d767d5b5719b16d
孟政元 <278227739@qq.com>
2017-05-10 15:26:42 +0800

    继续词法分析器的构建

--- word_analyse.js
+++ word_analyse.js
@@ -2,13 +2,13 @@ module.exports = function(str)
 {
     str = str.split('');
     let result = [];
+    let row = 1;
+    result.push([]);
     let char = str.shift();
     let DFA = require('./DFA')();
     DFA.next();
     while(char)
     {
-        // console.log('--------------------------------');
-        // console.log(`input char : ${char}`);
         let output = DFA.next(char);
         // console.log(output);
         if(output.value === undefined)
@@ -27,9 +27,43 @@ module.exports = function(str)
             {
                 DFA.next();
                 result.push(output);
-                // console.log(output);
+                if(output.type === '标识符')
+                {
+                    console.log(output);
+                }
             }
         }
     }
-    console.log(result);
+    // console.log(result);
 };
+
+function build_token(info)
+{
+    switch(info.type)
+    {
+        case '标识符':
+        {
+            switch(info.token.toLowerCase())
+            {
+                case 'program':
+                {
+                    return {
+                        lex: 'PROGRAM'
+                    };
+                }
+                case 'var':
+                {
+                    return {
+                        lex: 'VAR'
+                    };
+                }
+                case 'integer':
+                {
+                    return {
+                        lex: 'INTEGER'
+                    }
+                }
+            }
+        }
+    }
+}
