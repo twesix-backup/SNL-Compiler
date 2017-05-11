cdaedd9c69269cccc0b9c2f2505eb58eb958574b
孟政元 <278227739@qq.com>
2017-05-11 23:28:52 +0800

    开始处理保留字

--- word_analyse.js
+++ word_analyse.js
@@ -1,41 +1,62 @@
-module.exports = function(str)
+const path = require('path');
+
+module.exports = function(source_code)
 {
-    str = str.split('');
-    let result = [];
-    let row = 1;
-    result.push([]);
-    let char = str.shift();
     let DFA = require('./DFA')();
+    let rows = source_code.split(path.sep);
+    let row_number = 1;
+    let result = [];
+    result.push([]);
+
+    let row = rows.shift();
     DFA.next();
-    while(char)
+
+    while(row)
     {
-        let output = DFA.next(char);
-        // console.log(output);
-        if(output.value === undefined)
+        row = row.split('');
+        let char = row.shift();
+        while(char)
         {
-            char = str.shift();
-        }
-        else
-        {
-            output = JSON.parse(output.value);
-            if(output.type === '出错')
+            let output = DFA.next(char);
+            // console.log(output);
+            if(output.value === undefined)
             {
-                console.error(`Error : ${output.value.token}`);
-                return false;
+                char = row.shift();
             }
             else
             {
-                DFA.next();
-                result.push(output);
-                if(output.type === '标识符')
+                output = JSON.parse(output.value);
+                if(output.type === '出错')
                 {
-                    console.log(output);
+                    console.error(`Error : ${output.value.token}`);
+                    return false;
+                }
+                else
+                {
+                    DFA.next();
+                    result.push(output);
+                    if(output.type === '标识符')
+                    {
+                        console.log(output);
+                    }
                 }
             }
         }
+        row = rows.shift();
+        row_number ++;
     }
+
     // console.log(result);
 };
+
+let reserved_words =
+    [
+        'program',
+        'var',
+        'integer',
+        'array',
+        'of',
+    ];
 
 function build_token(info)
 {
